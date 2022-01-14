1c311788fbc952e3fb5e4b904b832ae35530f4bf
Gianluca Bianco <gianluca.bianco3@studio.unibo.it>
2022-01-14 16:35:22 +0100

    Added an aliases header. Added a namespace. Structure of the code has been updated and constness has been added to many variables.

---
+++ include/aliases.hpp
@@ -0,0 +1,35 @@
+#ifndef ALIASES_HPP
+#define ALIASES_HPP
+
+//STD headers
+#include <string>
+#include <complex>
+
+//Extra headers
+#include <exprtk.hpp>
+
+namespace safd
+ {
+  //============================================
+  //     Aliases for normal types
+  //============================================
+  using d_const = const double;
+  using i_const = const int;
+  using ui_const = const unsigned int;
+  
+  //============================================
+  //     Aliases for STD types
+  //============================================
+  using s_const = const std::string;
+  using cmplx = std::complex<double>;
+  using cmplx_const = const std::complex<double>;
+  
+  //============================================
+  //     Aliases for exprtk library
+  //============================================
+  typedef exprtk::symbol_table<double> symbol_table_t;
+  typedef exprtk::expression<double>     expression_t;
+  typedef exprtk::parser<double>             parser_t;
+ }
+
+#endif
